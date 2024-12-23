01dc7036ef6ec7d95e41f960c9be221573b152ce
Jehiel <jehiel.py@gmail.com>
2024-12-23 15:13:43 +0100

    Refactorizacion de codigo

--- include/pipex.h
+++ include/pipex.h
@@ -1,48 +1,76 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   pipex.h                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jlinarez <jlinarez@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/12/23 14:58:24 by jlinarez          #+#    #+#             */
+/*   Updated: 2024/12/23 15:11:08 by jlinarez         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #ifndef PIPEX_H
-#define PIPEX_H
+# define PIPEX_H
 
-#include <stdio.h>
-#include <stdlib.h>
-#include <unistd.h>
-#include <fcntl.h>
-#include <string.h>
-#include <sys/wait.h>
-#include <errno.h>
-#include "../utils/libft/libft.h"
-#include "../utils/ft_printf_42/ft_printf.h"
+# include <stdlib.h>
+# include <unistd.h>
+# include <fcntl.h>
+# include <stdio.h>
+# include <string.h>
+# include <sys/wait.h>
+# include <errno.h>
 
-
+// Estructura principal del proyecto
 typedef struct s_pipex
 {
-    int in_fd;               // Descriptor de archivo de entrada
-    int out_fd;              // Descriptor de archivo de salida
-    int here_doc;            // Flag para indicar si se usa here_doc
-    int is_invalid_infile;   // Flag para indicar si el archivo de entrada es inválido
-    char **cmd_paths;        // Matriz de rutas de comandos
-    char ***cmd_args;        // Matriz de argumentos de comandos
-    int cmd_count;           // Contador de comandos
-} t_pipex;
+    int     in_fd;          // Descriptor de archivo de entrada
+    int     out_fd;         // Descriptor de archivo de salida
+    int     fd[2];          // Pipe para redirección
+    int     prev_fd;        // Descriptor de archivo del comando anterior
+    int     cmd_count;      // Número de comandos
+    int     cmd_index;      // Índice del comando actual
+    char    **cmd_paths;    // Rutas de los comandos
+    char    **cmd_args;     // Argumentos de los comandos
+    char    **commands;     // Lista de comandos
+    char    **envp;         // Variables de entorno
+    char    outfile;
+    char    infile;
+}   t_pipex;
 
-// Funciones para manejar la ejecución de comandos
-void execute_command(const char *cmd, int input_fd, int output_fd);
+// Funciones principales
+void    ft_init_pipex(t_pipex *pipex);
+void    process_commands(t_pipex *pipex);
+void    handle_here_doc(int argc, char **argv, char **envp);
+void    parse_commands(t_pipex *pipex, char **argv, char **envp);
+void    execute_command_with_path(char *command, t_pipex *pipex);
 
-// Funciones de manejo de errores
-void handle_error(const char *msg);
-void print_error(const char *format, ...);
-void free_resources(t_pipex *pipex);
+// Manejo de errores
+void    handle_error(const char *msg);
+void    print_error(const char *format, ...);
+void    display_argument_error(void);
 
-// Funciones para inicializar y liberar recursos
-void ft_init_pipex(t_pipex *pipex);
-void free_2d_array(char **array);
-void parse_commands(t_pipex *pipex, char **argv, char **envp);
-void exec_pipex(t_pipex *pipex);
-void init_command_storage(t_pipex *pipex);
-void process_command(t_pipex *pipex, char *command, char **envp, int index);
-char *find_command_path(const char *cmd, char **envp);
-void free_command_storage(t_pipex *pipex);
-char *get_path_env(char **envp);
-char *check_command_in_paths(char **paths, const char *command);
+// Gestión de recursos
+void    clean_up(char **args, char *path);
+void    free_resources(t_pipex *pipex);
+void    close_fds(int fd[2], int prev_fd);
+void    free_2d_array(char **array);
 
+// Búsqueda de rutas y comandos
+char    *find_command_path(const char *cmd, char **envp);
+char    *get_path_env(char **envp);
+char    *check_command_in_paths(char **paths, const char *command);
 
+// Utilidades para here_doc
+void    read_from_stdin_and_write_to_pipe(char **argv, int fd[2]);
+int     open_outfile(char **argv, int argc);
 
-#endif // PIPEX_H
+// Funciones adicionales de tu librería (libft o propias)
+char    **ft_split(char const *s, char c);
+char    *ft_strjoin(char const *s1, char const *s2);
+int     ft_strncmp(const char *s1, const char *s2, size_t n);
+void    ft_putstr_fd(char *s, int fd);
+char    *ft_strdup(const char *s1);
+char    *get_next_line(int fd);
+
+#endif
--- src/heredoc.c
+++ src/heredoc.c
@@ -6,11 +6,11 @@
 /*   By: jlinarez <jlinarez@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/23 15:00:16 by jlinarez          #+#    #+#             */
-/*   Updated: 2024/12/23 15:01:07 by jlinarez         ###   ########.fr       */
+/*   Updated: 2024/12/23 15:11:31 by jlinarez         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "pipex.h"
+#include "../include/pipex.h"
 
 void handle_here_doc(int argc, char **argv, char **envp)
 {
@@ -31,4 +31,21 @@ void handle_here_doc(int argc, char **argv, char **envp)
     pipex.cmd_index = 0;
     process_commands(&pipex);
     close(here_doc_pipe[0]);
+}
+static void read_from_stdin_and_write_to_pipe(char **argv, int fd[2])
+{
+    char *line;
+
+    while (1)
+    {
+        ft_putstr_fd("pipe heredoc> ", 1);
+        line = get_next_line(STDIN_FILENO);
+        if (ft_strncmp(line, argv[2], ft_strlen(argv[2])) == 0
+            && line[ft_strlen(argv[2])] == '\n')
+            break;
+        ft_putstr_fd(line, fd[1]);
+        clean_up(NULL, line);
+    }
+    clean_up(NULL, line);
+    close(fd[1]);
 }
